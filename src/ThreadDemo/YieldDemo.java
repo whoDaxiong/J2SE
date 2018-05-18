765b6461a28b21b45ef33034d6c5c4ccf6d045d5
https://github.com/whoDaxiong/mygit <794313348@qq.com>
2018-05-18 19:51:22 +0800

    test

---
+++ src/ThreadDemo/YieldDemo.java
@@ -0,0 +1,13 @@
+package ThreadDemo;
+
+public class YieldDemo implements Runnable{
+
+	@Override
+	public void run() {
+		for(int i = 0;i<100;i++){
+			System.out.println(Thread.currentThread().getName()+"---"+i);
+			Thread.yield();
+		}
+	}
+
+}
---
+++ src/algorithm/Insert.java
@@ -0,0 +1,21 @@
+package algorithm;
+
+import java.util.Arrays;
+
+public class Insert {
+	public static void main(String[] args) {
+		int[] sz = { 45, 5, 7, 5, 4, 6, 7, 65, 4, 3, 89, 7 };
+
+		// 外层循环控制需要排序的趟数(从1开始因为将第0位看成了有序数据)
+		for (int i = 1; i < sz.length; i++) {
+			int temp = sz[i];
+			while (i > 0 && sz[i - 1] > temp) {
+				//插入到当前
+				sz[i] = sz[i-1];
+				i--;
+			}
+			sz[i]=temp;
+			System.out.println(Arrays.toString(sz));
+		}
+	}
+}
